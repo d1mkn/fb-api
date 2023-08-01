812532047e0fa09fb95e53156ddb7ee0c01b36e0
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-08-01 19:26:17 +0300

    example

---
+++ script.js
@@ -0,0 +1,47 @@
+let ukPosts = [];
+
+function getFacebookPosts() {
+  const baseUrl = "https://graph.facebook.com";
+  const pageId = "100563959805362";
+  const accessToken =
+    "";
+  const endpoint = `/${pageId}/feed`;
+
+  const url = `${baseUrl}${endpoint}?fields=permalink_url,message&access_token=${accessToken}`;
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Request failed with status ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      data.data.forEach((element) => {
+        if (element.message) {
+          const post = element.message;
+          const textFrame = post.slice(0, 10);
+          const enTextOnly = textFrame.replace(/[^a-zA-Z\s]/g, "").trim();
+          if (enTextOnly.length <= 0) {
+            ukPosts.push(element);
+          }
+        }
+      });
+    })
+    .then(() => {
+      // render
+      const posts = document.querySelectorAll(".fb-post iframe");
+      let counter = 0;
+      posts.forEach((post) => {
+        post.setAttribute(
+          "src",
+          `https://www.facebook.com/plugins/post.php?href=${ukPosts[counter].permalink_url}`
+        );
+        counter += 1;
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+getFacebookPosts();
